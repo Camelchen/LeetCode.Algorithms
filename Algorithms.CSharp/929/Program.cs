ed7927c2b42d681f3cd89555c85f5fbc65167830
Camel <camelchen@qq.com>
2019-02-15 21:41:07 -0500

    easy.929

---
+++ Algorithms.CSharp/929/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace _929
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] emails = new string[] {"test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"};
+            Console.WriteLine(NumUniqueEmails(emails));
+            Console.Read();
+        }
+
+        public static int NumUniqueEmails(string[] emails)
+        {
+            Hashtable uniqEmails = new Hashtable();
+
+            foreach (string email in emails)
+            {
+                string uniqEmail = email.Split("@")[0].Split("+")[0].Replace(".", "") + email.Split("@")[1];
+                if (!uniqEmails.Contains(uniqEmail))
+                {
+                    uniqEmails.Add(uniqEmail,1);
+                }
+            }
+
+            return uniqEmails.Count;
+        }
+    }
+}
