b21f6fdffa96574c4f15689188c597dc865e8883
Camel <camelchen@qq.com>
2019-02-16 09:11:54 -0500

    easy.728

---
+++ Algorithms.CSharp/728/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _728
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            foreach (var item in SelfDividingNumbers(1, 22))
+            {
+                Console.Write(item+",");
+
+            }
+            Console.Read();
+        }
+
+        public static IList<int> SelfDividingNumbers(int left, int right)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = left; i <= right; i++)
+            {
+                if (i.ToString().Contains("0")) { continue; }
+                bool divisible = true;
+                int num = i;
+                while (num!=0)
+                {
+                    int r = num % 10;
+                    if (i % r !=0) { divisible = false; break; }
+                    num = num / 10;
+                }
+                if (divisible) { 
+                    result.Add(i);
+                }
+            }
+            return (result);
+
+        }
+    }
+}
